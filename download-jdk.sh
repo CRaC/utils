126b875822197e768d2a5608b35c096bdf441151
Anton Kozlov <akozlov@azul.com>
2020-06-26 21:29:08 +0300

    Update

--- download-jdk.sh
+++ download-jdk.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
-id=$(curl -H "Authorization: token $GITHUB_TOKEN" \
-	https://api.github.com/repos/org-crac/jdk/releases/tags/release-crac | \
+REL=${1:-javax-crac}
+
+id=$(curl https://api.github.com/repos/org-crac/jdk/releases/tags/release-$REL | \
        	jq '.assets[] | select(.name == "jdk.tar.gz").id')
-curl -H "Authorization: token $GITHUB_TOKEN" \
-	-LJOH 'Accept: application/octet-stream' \
+curl -LJOH 'Accept: application/octet-stream' \
 	https://api.github.com/repos/org-crac/jdk/releases/assets/$id 
 
