b00b78c7f8e666d147474811db34ae069864a361
Anton Kozlov <akozlov@azul.com>
2020-07-27 20:25:04 +0300

    jdk -> jdk14-crac

--- download-jdk.sh
+++ download-jdk.sh
@@ -3,7 +3,7 @@
 REL=${1:-javax-crac}
 
 id=$(curl https://api.github.com/repos/org-crac/jdk/releases/tags/release-$REL | \
-       	jq '.assets[] | select(.name == "jdk.tar.gz").id')
+        jq '.assets[] | select(.name == "jdk14-crac.tar.gz").id')
 curl -LJOH 'Accept: application/octet-stream' \
-	https://api.github.com/repos/org-crac/jdk/releases/assets/$id 
+        https://api.github.com/repos/org-crac/jdk/releases/assets/$id
 
