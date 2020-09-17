d6443aaa3c0640f4974ad7f5b781bf1ab6a19f10
Anton Kozlov <akozlov@azul.com>
2020-09-17 17:44:31 +0300

    Handle redirects for download-jdk.sh

--- download-jdk.sh
+++ download-jdk.sh
@@ -2,7 +2,7 @@
 
 REL=${1:-javax-crac}
 
-id=$(curl https://api.github.com/repos/org-crac/jdk/releases/tags/release-$REL | \
+id=$(curl -L https://api.github.com/repos/org-crac/jdk/releases/tags/release-$REL | \
         jq '.assets[] | select(.name == "jdk14-crac.tar.gz").id')
 curl -LJOH 'Accept: application/octet-stream' \
         https://api.github.com/repos/org-crac/jdk/releases/assets/$id
