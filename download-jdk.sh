c84eb9ccefb29a94b317c7096d7f4224f1dae935
Anton Kozlov <akozlov@azul.com>
2022-01-18 20:19:14 +0300

    Update download-jdk

--- download-jdk.sh
+++ download-jdk.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-REL=${1:-javax-crac}
+BASE=https://api.github.com/repos/CRaC/openjdk-builds
 
-id=$(curl -L https://api.github.com/repos/org-crac/jdk/releases/tags/release-$REL | \
-        jq '.assets[] | select(.name == "jdk14-crac.tar.gz").id')
+release="$(curl -sL $BASE/releases/latest)"
+asset="$(echo $release | jq '.assets[] | select(.name | test("jdk[0-9]+-crac\\+[0-9]+\\.tar\\.gz"))')"
+id=$(echo $asset | jq .id)
+
 curl -LJOH 'Accept: application/octet-stream' \
-        https://api.github.com/repos/org-crac/jdk/releases/assets/$id
+        $BASE/releases/assets/$id >&2
 
+echo $asset | jq -r .name
